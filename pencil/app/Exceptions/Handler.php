8d0ee67a6871af18a764584523a96ccecb32f2b8
hato6502 <erisuke12345@yahoo.co.jp>
2019-07-22 22:52:02 +0900

    Laravel Sentry を導入しました。
    Laradock を使用。
    
    改行の調整。
    
    Sentry のデバッグコードを削除。

--- pencil/app/Exceptions/Handler.php
+++ pencil/app/Exceptions/Handler.php
@@ -31,6 +31,10 @@ class Handler extends ExceptionHandler
      */
     public function report(Exception $exception)
     {
+        if (!app()->isLocal() && app()->bound('sentry') && $this->shouldReport($exception)) {
+            app('sentry')->captureException($exception);
+        }
+
         parent::report($exception);
     }
 
