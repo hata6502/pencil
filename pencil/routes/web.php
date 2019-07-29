ed3d7a55f50ba290bcfd7d9761af96e49a2694b3
hato6502 <hato6502@gmail.com>
2019-07-30 00:29:24 +0900

    Twitter 認証のCallbackをフルパスで指定しました。
    Twitter 情報のデバッグ出力。
    
    DB に記録しない一時的なTwitter認証を導入しました。
    
    Twitter ログイン/ログアウトを実装しました。

--- pencil/routes/web.php
+++ pencil/routes/web.php
@@ -11,9 +11,14 @@
 |
 */
 
-Route::get('', 'TopController@draw')->name('draw');
+Route::get('', 'TopController@draw')->name('top');
 
 Route::group(['middleware' => 'auth.very_basic'], function () {
     Route::get('login', 'Auth\LoginController@login')->name('login');
     Route::get('login/callback', 'Auth\LoginController@callback')->name('callback');
+    Route::get('logout', 'Auth\LoginController@logout')->name('logout');
+});
+
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('post', 'TopController@draw')->name('post');
 });
