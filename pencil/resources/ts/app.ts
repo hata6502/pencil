cbff3233401576420b96e0344bae09426749855f
hato6502 <erisuke12345@yahoo.co.jp>
2019-08-07 01:40:46 +0900

    prettier 一括整形。

--- pencil/resources/ts/app.ts
+++ pencil/resources/ts/app.ts
@@ -1,99 +1,95 @@
-import 'core-js/stable'
-import * as Sentry from '@sentry/browser'
+import 'core-js/stable';
+import * as Sentry from '@sentry/browser';
 
-import DrawingWindow from './drawing-window'
-import DrawingDialog from './drawing-dialog'
-import DrawingCanvas from './drawing-canvas'
-import PencilButton from './pencil-button'
-import PaletteButton from './palette-button'
-import HistoryButton from './history-button'
-import StickCursor from './stick-cursor'
-import PointerListener from './pointer-listener'
-import TextInput from './text-input'
-import PreviewCanvas from './preview-canvas'
-import * as Settings from './settings'
+import DrawingWindow from './drawing-window';
+import DrawingDialog from './drawing-dialog';
+import DrawingCanvas from './drawing-canvas';
+import PencilButton from './pencil-button';
+import PaletteButton from './palette-button';
+import HistoryButton from './history-button';
+import StickCursor from './stick-cursor';
+import PointerListener from './pointer-listener';
+import TextInput from './text-input';
+import PreviewCanvas from './preview-canvas';
+import * as Settings from './settings';
 
-
-Sentry.init({ dsn: 'https://19e4397c2e5c47279823b887f7c74444@sentry.io/1509084' })
+Sentry.init({ dsn: 'https://19e4397c2e5c47279823b887f7c74444@sentry.io/1509084' });
 
 const drawingWindow = new DrawingWindow(
     <HTMLDivElement>document.getElementById('drawing-window'),
     () => {
-        drawingCanvas.isDisplay = true
+        drawingCanvas.isDisplay = true;
     },
     () => {
-        drawingCanvas.isDisplay = false
-        previewCanvas.setDrawing(drawingCanvas.getDrawing())
+        drawingCanvas.isDisplay = false;
+        previewCanvas.setDrawing(drawingCanvas.getDrawing());
     }
-)
+);
 
-new DrawingDialog(<HTMLDivElement>document.getElementById('drawing-dialog'))
+new DrawingDialog(<HTMLDivElement>document.getElementById('drawing-dialog'));
 
-const drawingCanvas = new DrawingCanvas(<HTMLCanvasElement>document.getElementById('drawing-canvas'))
+const drawingCanvas = new DrawingCanvas(<HTMLCanvasElement>document.getElementById('drawing-canvas'));
 drawingCanvas.onchangehistory = (index, length) => {
-    undoButton.setDisabled(index <= 0)
-    redoButton.setDisabled(index >= length - 1)
-}
+    undoButton.setDisabled(index <= 0);
+    redoButton.setDisabled(index >= length - 1);
+};
 
-let pencilButtons: PencilButton[] = []
+let pencilButtons: PencilButton[] = [];
 Array.prototype.forEach.call(
     document.getElementsByClassName('pencil-button'),
     (pencilButtonElement: HTMLButtonElement) => {
         const pencilButton = new PencilButton(pencilButtonElement, () => {
             pencilButtons.forEach((pencilButton: PencilButton) => {
-                pencilButton.inactivate()
-            })
+                pencilButton.inactivate();
+            });
 
-            pencilButton.activate()
-            drawingCanvas.brush = pencilButton.getBrush()
+            pencilButton.activate();
+            drawingCanvas.brush = pencilButton.getBrush();
 
-            textInput.inactivate()
-            drawingCanvas.mode = 'pencil'
-        })
+            textInput.inactivate();
+            drawingCanvas.mode = 'pencil';
+        });
 
-        pencilButtons.push(pencilButton)
+        pencilButtons.push(pencilButton);
     }
-)
+);
 
-let paletteButtons: PaletteButton[] = []
+let paletteButtons: PaletteButton[] = [];
 Array.prototype.forEach.call(
     document.getElementsByClassName('palette-button'),
     (paletteButtonElement: HTMLButtonElement) => {
         const paletteButton = new PaletteButton(paletteButtonElement, (color: string) => {
             paletteButtons.forEach((paletteButton: PaletteButton) => {
-                paletteButton.inactivate()
-            })
+                paletteButton.inactivate();
+            });
 
-            paletteButton.activate()
-            drawingCanvas.color = color
-        })
+            paletteButton.activate();
+            drawingCanvas.color = color;
+        });
 
-        paletteButtons.push(paletteButton)
+        paletteButtons.push(paletteButton);
     }
-)
+);
 
 const undoButton = new HistoryButton(<HTMLButtonElement>document.getElementById('undo-button'), () => {
-    drawingCanvas.undo()
-})
+    drawingCanvas.undo();
+});
 
 const redoButton = new HistoryButton(<HTMLButtonElement>document.getElementById('redo-button'), () => {
-    drawingCanvas.redo()
-})
+    drawingCanvas.redo();
+});
 
-const stickCursor = new StickCursor(
-    <HTMLDivElement>document.getElementById('stick-cursor')
-)
+const stickCursor = new StickCursor(<HTMLDivElement>document.getElementById('stick-cursor'));
 stickCursor.onmove = () => {
-    drawingCanvas.movePath(Math.floor(stickCursor.x), Math.floor(stickCursor.y))
+    drawingCanvas.movePath(Math.floor(stickCursor.x), Math.floor(stickCursor.y));
 };
 stickCursor.onend = () => {
-    drawingCanvas.finishPath()
+    drawingCanvas.finishPath();
 };
-
 
 const pointerListener = new PointerListener();
 pointerListener.onstart = () => {
-    stickCursor.hide()
+    stickCursor.hide();
 };
 pointerListener.onmove = (screenX, screenY) => {
     // ポインターのスクリーン座標を drawingCanvas との相対座標に変換します。
@@ -101,18 +97,18 @@ pointerListener.onmove = (screenX, screenY) => {
     const canvasX = Math.floor((screenX - 2 - canvasPosition.x) / Settings.CANVAS_ZOOM);
     const canvasY = Math.floor((screenY - 2 - canvasPosition.y) / Settings.CANVAS_ZOOM);
 
-    drawingCanvas.movePath(canvasX, canvasY)
+    drawingCanvas.movePath(canvasX, canvasY);
 };
 pointerListener.onend = () => {
-    drawingCanvas.finishPath()
+    drawingCanvas.finishPath();
 };
 
-const textInput = new TextInput(<HTMLInputElement>document.getElementById('text-input'))
+const textInput = new TextInput(<HTMLInputElement>document.getElementById('text-input'));
 textInput.onactive = text => {
-    drawingCanvas.text = text
-    drawingCanvas.mode = 'text'
-}
+    drawingCanvas.text = text;
+    drawingCanvas.mode = 'text';
+};
 
 const previewCanvas = new PreviewCanvas(<HTMLCanvasElement>document.getElementById('preview-canvas'), () => {
-    drawingWindow.display()
-})
+    drawingWindow.display();
+});
