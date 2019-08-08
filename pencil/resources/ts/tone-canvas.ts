45b8758daf2dcdc65eff3482e27faf4cb5d2f2f8
hato6502 <erisuke12345@yahoo.co.jp>
2019-08-09 00:30:23 +0900

    トーンボタンを作成しました。
    canvaas要素を取得できないバグを修正。
    
    テーブルレイアウトを使わないように改善。

---
+++ pencil/resources/ts/tone-canvas.ts
@@ -0,0 +1,35 @@
+import * as Settings from './settings';
+
+export default class {
+    private element: HTMLCanvasElement;
+    private context: CanvasRenderingContext2D;
+    private tone: number[][] = Settings.TONES.black;
+
+    constructor(element: HTMLCanvasElement) {
+        this.element = element;
+        const context = this.element.getContext('2d');
+        if (context === null) {
+            throw "Couldn't get context. ";
+        }
+        this.context = context;
+
+        this.draw();
+    }
+
+    setTone(tone: number[][]): void {
+        this.tone = tone;
+        this.draw();
+    }
+
+    private draw(): void {
+        this.element.setAttribute('width', (this.tone.length * 4).toString());
+        this.element.setAttribute('height', (this.tone.length * 4).toString());
+
+        this.tone.forEach((column, y) => {
+            column.forEach((pattern, x) => {
+                this.context.fillStyle = pattern ? 'black' : 'white';
+                this.context.fillRect(x * 4, y * 4, 4, 4);
+            });
+        });
+    }
+}
