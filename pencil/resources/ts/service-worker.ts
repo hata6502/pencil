2103f58464bc1d95b08e4911fce7d359c3381b61
hato6502 <erisuke12345@yahoo.co.jp>
2019-08-11 22:58:01 +0900

    ServiceWorker のバージョン変更。

--- pencil/resources/ts/service-worker.ts
+++ pencil/resources/ts/service-worker.ts
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'hood-pencil-cache-v0.1.4';
+const CACHE_NAME = 'hood-pencil-cache-v0.1.5';
 const URLS_TO_CACHE = ['/', '/js/app.js', '/css/app.css'];
 
 self.addEventListener('install', (event: any) => {
