264663bab95bb499f02937ad59293b81e73d3106
hato6502 <erisuke12345@yahoo.co.jp>
2019-08-19 12:54:52 +0900

    FileReader が使える環境では、サーバーサイドで DataURL 変換させないようにしました。
    Wii U では FileReader を使わないように修正。

--- pencil/resources/ts/drawing-window/background-file.ts
+++ pencil/resources/ts/drawing-window/background-file.ts
@@ -2,6 +2,7 @@ import VirtualElement from '../virtual-element';
 
 export default class extends VirtualElement<HTMLInputElement> {
     onSelect: () => void = () => {};
+    onLoad: (image: HTMLImageElement) => void = () => {};
 
     constructor(element: HTMLInputElement) {
         super(element);
@@ -9,7 +10,30 @@ export default class extends VirtualElement<HTMLInputElement> {
         this.element.onchange = () => {
             if (this.element.files !== null && this.element.files.length == 1) {
                 this.onSelect();
+
+                if ('FileReader' in window && navigator.userAgent.toLowerCase().indexOf('nintendo wiiu') == -1) {
+                    const reader = new FileReader();
+
+                    reader.onloadend = () => {
+                        if (reader.error !== null) {
+                            throw reader.error;
+                        }
+                        if (typeof reader.result != 'string') {
+                            throw 'Unexpected result type. ';
+                        }
+
+                        const image = new Image();
+                        image.src = reader.result;
+                        image.onload = () => {
+                            this.onLoad(image);
+                        };
+                    };
+                    reader.readAsDataURL(this.element.files[0]);
+                }
             }
+
+            // 同じファイルを選択しても onchange イベントを発火させるようにします。
+            element.value = '';
         };
     }
 
