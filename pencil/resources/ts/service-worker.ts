c64cb14bc282e46b4fc015bd917ec1d39c7e3192
hato6502 <erisuke12345@yahoo.co.jp>
2019-09-01 18:15:48 +0900

    ServiceWorker のキャッシュを手動設定しました。

--- pencil/resources/ts/service-worker.ts
+++ pencil/resources/ts/service-worker.ts
@@ -1,5 +1,54 @@
 const CACHE_NAME = 'hood-pencil-cache-v0.2.1';
-const URLS_TO_CACHE = ['/', '/js/app.js', '/css/app.css'];
+const URLS_TO_CACHE = [
+    '/',
+    '/draw',
+
+    '/js/app.js',
+
+    '/css/app.css',
+
+    '/images/favicon@32px.png',
+    '/images/favicon@192px.png',
+    '/images/wing.png',
+    '/images/ground.png',
+    '/images/image.png',
+    '/images/clear.png',
+    '/images/undo.png',
+    '/images/redo.png',
+    '/images/transparent.png',
+    '/images/pencil-light.png',
+    '/images/pencil-medium.png',
+    '/images/pencil-bold.png',
+    '/images/background-white.png',
+    '/images/background-file.png',
+    '/images/background-wide.png',
+
+    '/images/animals/お魚.png',
+    '/images/animals/イノシシ.png',
+    '/images/animals/カエル.png',
+    '/images/animals/コブラ.png',
+    '/images/animals/ツチノコ.png',
+    '/images/animals/フクロウ.png',
+    '/images/animals/海老.png',
+    '/images/animals/猫.png',
+    '/images/animals/イカ.png',
+    '/images/animals/ウサギ.png',
+    '/images/animals/カバ.png',
+    '/images/animals/ゾウ.png',
+    '/images/animals/ネズミ.png',
+    '/images/animals/リス.png',
+    '/images/animals/豚.png',
+    '/images/animals/馬.png',
+
+    '/images/landscapes/タージマハル.png',
+    '/images/landscapes/チチェン・イッツァ.png',
+    '/images/landscapes/ピサの斜塔.png',
+    '/images/landscapes/ピラミッド.png',
+    '/images/landscapes/街.png',
+    '/images/landscapes/教会.png',
+    '/images/landscapes/城.png',
+    '/images/landscapes/富士山.png'
+];
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 self.addEventListener('install', (event: any): void => {
@@ -43,16 +92,6 @@ self.addEventListener('fetch', (event: any): void => {
 
             return fetch(fetchedRequest).then(
                 (response): Response => {
-                    if (!response || response.status !== 200 || response.type !== 'basic') {
-                        return response;
-                    }
-
-                    const cachedResponse = response.clone();
-
-                    caches.open(CACHE_NAME).then((cache): void => {
-                        cache.put(event.request, cachedResponse);
-                    });
-
                     return response;
                 }
             );
